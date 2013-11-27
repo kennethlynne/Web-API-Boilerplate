6dd80f3e0ef1cfbadd8402715c37a063f246f95e
Kenneth Lynne <kenneth.lynne@gmail.com>
2013-11-27 19:20:31 +0100

    Only support attribute routing

--- Boilerplate/Boilerplate/App_Start/WebApiConfig.cs
+++ Boilerplate/Boilerplate/App_Start/WebApiConfig.cs
@@ -9,16 +9,7 @@ namespace Boilerplate
     {
         public static void Register(HttpConfiguration config)
         {
-            // Web API configuration and services
-
-            // Web API routes
             config.MapHttpAttributeRoutes();
-
-            config.Routes.MapHttpRoute(
-                name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
-            );
         }
     }
 }
