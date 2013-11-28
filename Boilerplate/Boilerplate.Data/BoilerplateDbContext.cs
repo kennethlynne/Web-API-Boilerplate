4b4c2b40ffab8c0f5c37c43a8878e757e0e2d0f7
Kenneth Lynne <kenneth.lynne@gmail.com>
2013-11-28 18:14:01 +0100

    Work on DAL

---
+++ Boilerplate/Boilerplate.Data/BoilerplateDbContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Boilerplate.Models;
+
+namespace Boilerplate.Data
+{
+    class BoilerplateDbContext : DbContext
+    {
+        public BoilerplateDbContext() : base("Boilerplate")
+        {
+            
+        }
+
+        DbSet<Message> Messages { get; set; }
+
+        static BoilerplateDbContext()
+        {
+            Database.SetInitializer(new DbInitializer());
+        }
+    }
+}
---
+++ Boilerplate/Boilerplate.Data/DbInitializer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boilerplate.Data
+{
+    class DbInitializer : DropCreateDatabaseAlways<BoilerplateDbContext>
+    {
+        protected override void Seed(BoilerplateDbContext context)
+        {
+        }
+    }
+}
