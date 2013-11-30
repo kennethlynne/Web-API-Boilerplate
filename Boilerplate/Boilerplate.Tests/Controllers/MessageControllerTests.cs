4247a4e7e804be3faf8e9b6a6e2bfbb408aff7e4
Kenneth Lynne <kenneth.lynne@gmail.com>
2013-11-30 22:13:00 +0100

    Implemented basic test with DI

---
+++ Boilerplate/Boilerplate.Tests/Controllers/MessageControllerTests.cs
@@ -0,0 +1,23 @@
+﻿using Boilerplate.Data;
+using Boilerplate.Data.Interfaces.Services;
+using Boilerplate.Tests.Mocks;
+using Boilerplate.Web.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Boilerplate.Tests.Controllers
+{
+    [TestClass]
+    public class MessageControllerTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            IMessagingService service = new MockMessagingService();
+            MessageController controller = new MessageController(service);
+            
+            var result = controller.GetMessage("Hello world");
+
+            Assert.AreEqual("Hello world, your mock says hi!", result.Text);
+        }
+    }
+}
