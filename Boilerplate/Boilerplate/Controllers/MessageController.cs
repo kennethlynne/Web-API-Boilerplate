eb2d6e110f82f8a3a6051e3dfa35c4a44ec7a5fa
Kenneth Lynne <kenneth.lynne@gmail.com>
2013-11-30 21:14:07 +0100

    Added API documentation

--- Boilerplate/Boilerplate/Controllers/MessageController.cs
+++ Boilerplate/Boilerplate/Controllers/MessageController.cs
@@ -4,6 +4,9 @@ using Boilerplate.Web.CORS;
 
 namespace Boilerplate.Web.Controllers
 {
+    /// <summary>
+    /// Messages
+    /// </summary>
     [RoutePrefix("api")]
     //[EnableCors(origins: "http://www.example.com", headers: "accept,content-type,origin,x-my-header", methods: "get,post", SupportsCredentials = true)]
     [CustomCORS]
@@ -11,17 +14,29 @@ namespace Boilerplate.Web.Controllers
     {
         private readonly IMessagingService _messagingService;
 
+        /// <param name="messagingService"></param>
         public MessageController(IMessagingService messagingService)
         {
             _messagingService = messagingService;
         }
 
+
+        /// <summary>
+        /// Echo a message
+        /// </summary>
+        /// <param name="message"></param>
+        /// <returns></returns>
         [Route("messages/{message:minlength(2)}")]
         public string GetMessage(string message)
         {
             return message;
         }
 
+        /// <summary>
+        /// Echo message pluss a number
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         [Route("messages/{id:int}")]
         public string GetMessageById(int id)
         {
--- Boilerplate/Boilerplate/Global.asax.cs
+++ Boilerplate/Boilerplate/Global.asax.cs
@@ -1,18 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Http;
-using System.Web.Routing;
+﻿using System.Web.Http;
+using System.Web.Mvc;
 using Boilerplate.Web.App_Start;
 
-namespace Boilerplate
+namespace Boilerplate.Web
 {
     public class WebApiApplication : System.Web.HttpApplication
     {
         protected void Application_Start()
         {
             GlobalConfiguration.Configure(WebApiConfig.Register);
+            AreaRegistration.RegisterAllAreas();
         }
     }
 }
