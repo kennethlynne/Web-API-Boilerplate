845645073065e37dc79f612c70a649384aa3e8c5
Kenneth Lynne <kenneth.lynne@gmail.com>
2013-12-01 03:07:50 +0100

    Cleanup

--- Boilerplate/Boilerplate/App_Start/NinjectWebCommon.cs
+++ Boilerplate/Boilerplate/App_Start/NinjectWebCommon.cs
@@ -1,36 +1,32 @@
+using Ninject.Syntax;
 using System.Web.Http;
 using System.Web.Http.Dependencies;
-using Boilerplate.Data;
-using Boilerplate.Data.Interfaces.Services;
-using Ninject.Syntax;
 
 [assembly: WebActivator.PreApplicationStartMethod(typeof(Boilerplate.Web.App_Start.NinjectWebCommon), "Start")]
 [assembly: WebActivator.ApplicationShutdownMethodAttribute(typeof(Boilerplate.Web.App_Start.NinjectWebCommon), "Stop")]
 
 namespace Boilerplate.Web.App_Start
 {
+    using Microsoft.Web.Infrastructure.DynamicModuleHelper;
+    using Ninject;
+    using Ninject.Web.Common;
     using System;
     using System.Web;
 
-    using Microsoft.Web.Infrastructure.DynamicModuleHelper;
-
-    using Ninject;
-    using Ninject.Web.Common;
-
-    public static class NinjectWebCommon 
+    public static class NinjectWebCommon
     {
         private static readonly Bootstrapper bootstrapper = new Bootstrapper();
 
         /// <summary>
         /// Starts the application
         /// </summary>
-        public static void Start() 
+        public static void Start()
         {
             DynamicModuleUtility.RegisterModule(typeof(OnePerRequestHttpModule));
             DynamicModuleUtility.RegisterModule(typeof(NinjectHttpModule));
             bootstrapper.Initialize(CreateKernel);
         }
-        
+
         /// <summary>
         /// Stops the application.
         /// </summary>
@@ -38,7 +34,7 @@ namespace Boilerplate.Web.App_Start
         {
             bootstrapper.ShutDown();
         }
-        
+
         /// <summary>
         /// Creates the kernel that will manage your application.
         /// </summary>
@@ -48,7 +44,7 @@ namespace Boilerplate.Web.App_Start
             var kernel = new StandardKernel();
             kernel.Bind<Func<IKernel>>().ToMethod(ctx => () => new Bootstrapper().Kernel);
             kernel.Bind<IHttpModule>().To<HttpApplicationInitializationHttpModule>();
-            
+
             RegisterServices(kernel);
             GlobalConfiguration.Configuration.DependencyResolver = new NinjectDependencyResolver(kernel);
             return kernel;
@@ -60,29 +56,33 @@ namespace Boilerplate.Web.App_Start
         /// <param name="kernel">The kernel.</param>
         private static void RegisterServices(IKernel kernel)
         {
-            kernel.Bind<IMessagingService>().To<MessagingService>();
-        }        
+            //kernel.Bind<UnitOfWork>().To<UnitOfWork>();
+        }
     }
 
     public class NinjectDependencyScope : IDependencyScope
     {
-        IResolutionRoot resolver;
+        private IResolutionRoot resolver;
+
         public NinjectDependencyScope(IResolutionRoot resolver)
         {
             this.resolver = resolver;
         }
+
         public object GetService(Type serviceType)
         {
             if (resolver == null)
                 throw new ObjectDisposedException("this", "This scope has been disposed");
             return resolver.TryGet(serviceType);
         }
+
         public System.Collections.Generic.IEnumerable<object> GetServices(Type serviceType)
         {
             if (resolver == null)
                 throw new ObjectDisposedException("this", "This scope has been disposed");
             return resolver.GetAll(serviceType);
         }
+
         public void Dispose()
         {
             IDisposable disposable = resolver as IDisposable;
@@ -91,17 +91,20 @@ namespace Boilerplate.Web.App_Start
             resolver = null;
         }
     }
+
     public class NinjectDependencyResolver : NinjectDependencyScope, IDependencyResolver
     {
-        IKernel kernel;
+        private IKernel kernel;
+
         public NinjectDependencyResolver(IKernel kernel)
             : base(kernel)
         {
             this.kernel = kernel;
         }
+
         public IDependencyScope BeginScope()
         {
             return new NinjectDependencyScope(kernel.BeginBlock());
         }
-    } 
+    }
 }
